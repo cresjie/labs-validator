14d07c5c90c3ba3fa365c835c2e73088043c3bea
Cres Jie <cresjie@gmail.com>
2015-09-24 21:18:32 +0800

    Update labs-validator.js

--- labs-validator.js
+++ labs-validator.js
@@ -17,12 +17,14 @@
 			var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
     		return re.test(val);
 		},
-		required: function(val){
+		required: function(val, par, name, element, helper, list, accessor){
+			
 			return val ? true : false;
 		},
-		requiredIf: function(val, par, name, element, helper){
+		requiredIf: function(val, par, name, element, helper, list, accessor){
+			
 			var data = par.split(":");
-			if( helper.getValueOf(data[0]) == data[1] ){ 
+			if( list[data[0]] == data[1] ){ 
 				return this.required(val);
 			}
 			return true;
@@ -34,8 +36,8 @@
 			var n = par.split(",");
 			return val >= n[0] && val <= n[1];
 		},
-		same: function(val,par,name,element,helper){
-			return val == helper.getValueOf(par);
+		same: function(val,par,name,element,helper, list, accessor){
+			return val == list[par];
 
 		},
 		_in: function(val,par){
@@ -132,6 +134,8 @@
 		}
 	};
 
+
+
 	function docReady(callback){
 		if( document.addEventListener){
 			document.addEventListener( "DOMContentLoaded",function(e){
@@ -147,6 +151,46 @@
 
 		}
 	}
+
+	var helper = {
+		
+		toDisplayableName: function(str, separator){
+			separator = typeof separator !== 'undefined' ? separator: '[\-_]';
+			var reg = new RegExp(separator + '.','g');
+			str = str.replace(/^./,function(g){return g.toUpperCase()});
+			str = str.replace(reg,function(g){return ' '+g[1].toUpperCase();});
+			return str;
+		},
+		toSnakeCase: function(str,separator){
+			separator = typeof separator !== 'undefined' ? separator: '[\-_]';
+			str = this.toCamelCase(str,separator);
+			return str;
+		},
+		toCamelCase: function(str,separator){
+			separator = typeof separator !== 'undefined' ? separator: '[\-_]';
+			var reg = new RegExp(separator + '([a-z])','g');
+			return str.replace(reg,function(g){return g[1].toUpperCase();});
+			
+		},
+		toDashCase: function(str,separator){
+			separator = typeof separator !== 'undefined' ? separator: '_';
+			return str.replace(/\s+/g,separator).toLowerCase();
+		},
+		copy: function(obj, obj2){
+			for(var k in obj2)
+				obj[k] = obj[k];
+			return obj;
+		},
+		addClass: function(el, className){
+
+			var classList = el.className.split(' ');
+			classList.push(className);
+			el.className = classList.join(' ');
+
+		
+			return this;
+		}
+	};
 	
  	window.labsValidator = function(){ //labsValidator class
 
@@ -160,52 +204,16 @@
 			attrPrefix: 'validator-'
 		};
 
-		var helper = {
+		var accessor = {
 			getValueOf: function(name){
-				if(form[name])
-					return form[name].value;
+			if(form[name])
+				return form[name].value;
 			},
 			getFiles: function(name){
 				if(form[name])
 					return form[name].files;
-			},
-			toDisplayableName: function(str, separator){
-				separator = typeof separator !== 'undefined' ? separator: '[\-_]';
-				var reg = new RegExp(separator + '.','g');
-				str = str.replace(/^./,function(g){return g.toUpperCase()});
-				str = str.replace(reg,function(g){return ' '+g[1].toUpperCase();});
-				return str;
-			},
-			toSnakeCase: function(str,separator){
-				separator = typeof separator !== 'undefined' ? separator: '[\-_]';
-				str = this.toCamelCase(str,separator);
-				return str;
-			},
-			toCamelCase: function(str,separator){
-				separator = typeof separator !== 'undefined' ? separator: '[\-_]';
-				var reg = new RegExp(separator + '([a-z])','g');
-				return str.replace(reg,function(g){return g[1].toUpperCase();});
-				
-			},
-			toDashCase: function(str,separator){
-				separator = typeof separator !== 'undefined' ? separator: '_';
-				return str.replace(/\s+/g,separator).toLowerCase();
-			},
-			copy: function(obj, obj2){
-				for(var k in obj2)
-					obj[k] = obj[k];
-				return obj;
-			},
-			addClass: function(el, className){
-
-				var classList = el.className.split(' ');
-				classList.push(className);
-				el.className = classList.join(' ');
-
-			
-				return this;
 			}
-		};
+		}
 		
 
 		function __construct(formId,opt){
@@ -262,7 +270,14 @@
 			passes: function(){
 				this.reset();
 				var passes = true,
-					elements = form.elements;
+					elements = form.elements,
+					list= {};
+
+				for(var i =0;i< elements.length; i++){
+					list[elements[i].name] = elements[i].value;
+				}
+
+				
 
 				for(var i =0;i < elements.length; i++){  //loop through each elements
 					
@@ -280,7 +295,7 @@
 					 			
 					 			//calls the validator function
 					 			// if validator fails set var passes to false 
-					 			if( !validators[validatorName](el.value, attr.value, helper.toDisplayableName(el.name), el, helper ) ){ 
+					 			if( !validators[validatorName](el.value, attr.value, helper.toDisplayableName(el.name), el, helper, list, accessor ) ){ 
 					 				passes = false;
 
 					 				if( !errors[i] ){ //if error object doesnt exists
@@ -292,7 +307,7 @@
 					 				}
 					 				
 					 				var messageRaw = validatorMessage[validatorName] || validatorMessage._default;
-					 				var	message = messageRaw.constructor === String ? messageRaw : messageRaw(el.value, attr.value, helper.toDisplayableName(el.name), el, helper);
+					 				var	message = messageRaw.constructor === String ? messageRaw : messageRaw(el.value, attr.value, helper.toDisplayableName(el.name), el, helper, list, accessor);
 
 					 				errors[i].messages.push(message);
 					 				errors[i].validatorName.push(helper.toDashCase(validatorName) );
@@ -320,6 +335,9 @@
 				errors = {};
 				removeErrors();
 				return this;
+			},
+			getErrors: function(){
+				return errors;
 			}
 		};
 		
@@ -333,6 +351,39 @@
 	window.labsValidator.addValidatorMsg = function(name, fn){
 		validatorMessage[name] = fn;
 		return this;
+	}
+
+	window.labsValidator.validate = function(attrs, rules){
+		var pass = true,errorMsg = {};
+		for(var name in rules){
+			var _validators = rules[name].split('|');
+			
+			for(var i in _validators){
+				var validatorRaw = _validators[i].split('=');
+				var validatorName = helper.toCamelCase(validatorRaw[0]);
+
+				if(validators[validatorName]){ //if validator name exists el.value, attr.value, helper.toDisplayableName(el.name), el, helper 
+					
+					if(!validators[validatorName]( attrs[name], validatorRaw[1], helper.toDisplayableName(name),null, helper, attrs, null )) {
+						pass = false;
+						if(!errorMsg[name])
+							errorMsg[name] = [];
+
+						var messageRaw = validatorMessage[validatorName] || validatorMessage._default;
+						var	message = messageRaw.constructor === String ? messageRaw : messageRaw( attrs[name], validatorRaw[1], helper.toDisplayableName(name),null, helper, attrs, null);
+						errorMsg[name].push(message)
+					} 
+				}
+			}
+		}
+
+		if(pass)
+			return pass;
+		return errorMsg;
+	}
+
+	labsValidator.test = function(){
+		console.log(123);
 	}
 
 	docReady(function(e){
